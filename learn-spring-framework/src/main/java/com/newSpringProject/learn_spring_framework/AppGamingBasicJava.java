bb94c9f8ff106bdc415733e1c51e3abe82dcc5f6
ANUJ PUJARI <anujpujari@gmail.com>
2025-09-09 20:08:48 +0530

    "Java Spring Boot Project"

---
+++ learn-spring-framework/src/main/java/com/newSpringProject/learn_spring_framework/AppGamingBasicJava.java
@@ -0,0 +1,19 @@
+package com.newSpringProject.learn_spring_framework;
+
+import com.newSpringProject.learn_spring_framework.game.GameRunner;
+import com.newSpringProject.learn_spring_framework.game.MarioGame;
+import com.newSpringProject.learn_spring_framework.game.SuperContraGame;
+
+public class AppGamingBasicJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	//	var game = new MarioGame();
+		var game = new SuperContraGame();
+		var gameRunner = new GameRunner(game);
+		gameRunner.run();
+
+	}
+
+}
